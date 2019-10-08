df28bf86a66ad4335395f038e5562cf6b7aaa438
Vitalii <36390409+g1yk@users.noreply.github.com>
2019-10-08 17:53:01 -0500

    Create phoneNumber.js

---
+++ phoneNumber.js
@@ -0,0 +1,7 @@
+const createPhoneNumber = (array) => {
+
+let result = `(${array[0]}${array[1]}${array[2]}) ${array[3]}${array[4]}${array[5]}-${array[6]}${array[7]}${array[8]}${array[9]}`
+
+  return result
+
+}
